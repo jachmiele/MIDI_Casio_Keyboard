dcbbe1b443de35c3cd771eb01d58d85c4c03dec1
Jacobo <43934145+jachmiele@users.noreply.github.com>
2021-06-21 16:44:07 +0200

    Add files via upload

---
+++ Code/Keyboard.ino
@@ -0,0 +1,130 @@
+#include <MIDI.h>
+
+struct MySettings : public midi::DefaultSettings
+{
+    static const long BaudRate = 115200;
+};
+
+// Create a 'MIDI' object using MySettings bound to Serial.
+MIDI_CREATE_CUSTOM_INSTANCE(HardwareSerial, Serial, MIDI, MySettings);
+
+#define NUM_ROWS 6
+#define NUM_COLS 9
+
+#define NOTE_ON_CMD 0x90
+#define NOTE_OFF_CMD 0x80
+#define NOTE_VELOCITY 127
+
+// Row input pins
+const int row1Pin = 7;
+const int row2Pin = 6;
+const int row3Pin = 5;
+const int row4Pin = 4;
+const int row5Pin = 3;
+const int row6Pin = 2;
+
+// 74HC595 pins
+const int dataPin = 8;
+const int latchPin = 9;
+const int clockPin = 10;
+
+boolean keyPressed[NUM_ROWS][NUM_COLS];
+uint8_t keyToMidiMap[NUM_ROWS][NUM_COLS];
+
+// bitmasks for scanning columns
+int bits[] =
+{ 
+  B00000001,
+  B00000010,
+  B00000100,
+  B00001000,
+  B00010000,
+  B00100000,
+  B01000000,
+  B10000000
+};
+
+void setup()
+{
+  int note = 36;
+
+  for(int colCtr = 0; colCtr < NUM_COLS; ++colCtr)
+  {
+    for(int rowCtr = 0; rowCtr < NUM_ROWS; ++rowCtr)
+    {
+      keyPressed[rowCtr][colCtr] = false;
+      keyToMidiMap[rowCtr][colCtr] = note;
+      note++;
+    }
+  }
+
+  // setup pins output/input mode
+  pinMode(dataPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(latchPin, OUTPUT);
+
+  pinMode(row1Pin, INPUT);
+  pinMode(row2Pin, INPUT);
+  pinMode(row3Pin, INPUT);
+  pinMode(row4Pin, INPUT);
+  pinMode(row5Pin, INPUT);
+  pinMode(row6Pin, INPUT);
+
+  //Serial.begin(SERIAL_RATE);
+  MIDI.begin();
+}
+
+void loop()
+{
+  for (int colCtr = 0; colCtr < NUM_COLS; ++colCtr)
+  {
+    //scan next column
+    scanColumn(colCtr);
+
+    //get row values at this column
+    int rowValue[NUM_ROWS];
+    rowValue[0] = digitalRead(row1Pin);
+    rowValue[1] = digitalRead(row2Pin);
+    rowValue[2] = digitalRead(row3Pin);
+    rowValue[3] = digitalRead(row4Pin);
+    rowValue[4] = digitalRead(row5Pin);
+    rowValue[5] = digitalRead(row6Pin);
+
+    // process keys pressed
+    for(int rowCtr=0; rowCtr<NUM_ROWS; ++rowCtr)
+    {
+      if(rowValue[rowCtr] != 0 && !keyPressed[rowCtr][colCtr])
+      {
+        keyPressed[rowCtr][colCtr] = true;
+        MIDI.sendNoteOn(keyToMidiMap[rowCtr][colCtr], 127, 1);
+      }
+    }
+
+    // process keys released
+    for(int rowCtr=0; rowCtr<NUM_ROWS; ++rowCtr)
+    {
+      if(rowValue[rowCtr] == 0 && keyPressed[rowCtr][colCtr])
+      {
+        keyPressed[rowCtr][colCtr] = false;
+        MIDI.sendNoteOff(keyToMidiMap[rowCtr][colCtr], 0, 1);
+      }
+    }
+  }
+}
+
+void scanColumn(int colNum)
+{
+  digitalWrite(latchPin, LOW);
+
+  if(0 <= colNum && colNum <= 7)
+  {
+    shiftOut(dataPin, clockPin, MSBFIRST, B00000000); //right sr
+    shiftOut(dataPin, clockPin, MSBFIRST, bits[colNum]); //left sr
+  }
+  else
+  {
+    shiftOut(dataPin, clockPin, MSBFIRST, bits[colNum-8]); //right sr
+    shiftOut(dataPin, clockPin, MSBFIRST, B00000000); //left sr
+  }
+  digitalWrite(latchPin, HIGH);
+}
